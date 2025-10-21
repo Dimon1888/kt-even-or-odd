b1ccf353347af03c9322153880ed4b8600de3c4b
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-21 21:55:42 +0200

    completed the task4

--- src/main/kotlin/mate/academy/EvenOrOdd.kt
+++ src/main/kotlin/mate/academy/EvenOrOdd.kt
@@ -1,11 +1,13 @@
 package mate.academy
-
-fun evenOrOdd(number: Int) = if (number % 2 == 0) {
+private const val DIVISOR_FOR_PARITY = 2
+private const val EVEN_REMAINDER = 0
+private const val EVEN_REMA = 10
+fun evenOrOdd(number: Int) = if (number % DIVISOR_FOR_PARITY  == EVEN_REMAINDER) {
     "Even"
 } else {
     "Odd"
 }
 
 fun main() {
-    println("10 e ${evenOrOdd(10)}")
+    println("10 e ${evenOrOdd(EVEN_REMA)}")
 }
