69dd409624599c9b2264dcbb7353b9625f85bd44
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-21 21:50:24 +0200

    completed the task2

--- src/main/kotlin/mate/academy/EvenOrOdd.kt
+++ src/main/kotlin/mate/academy/EvenOrOdd.kt
@@ -1,10 +1,5 @@
 package mate.academy
 
-//Створіть evenOrOdd()функцію, яка приймає ціле число як аргумент і повертає результат "Even"для парних або "Odd"непарних чисел.
-// Create a function `evenOrOdd` that takes an integer as an argument
-// and returns "Even" for eve
-// Створіть функцію `evenOrOdd`, яка приймає ціле число як аргумент
-// та повертає "Парний" для парних чисел або "Непарний" для непарних чисел.n numbers or "Odd" for odd numbers.
 fun evenOrOdd(number: Int) = if (number % 2 == 0) {
     "Even"
 } else {
@@ -12,5 +7,6 @@ fun evenOrOdd(number: Int) = if (number % 2 == 0) {
 }
 
 fun main() {
+    fun evenOrOdd(number: Int) = if (number % 2 == 0) {
     println("10 e ${evenOrOdd(10)}")
 }
